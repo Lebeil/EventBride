7e05d32b613b495cc24bfc04d4734ff009d96ae5
Lebeil <liedel75@gmail.com>
2020-05-13 00:38:13 +0200

    maj

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,10 @@ class EventsController < ApplicationController
   def index
     @all_events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'events#index'
   # devise_for :users
-  # root to: 'events#index'
-  # resources :events, only: [:new, :create, :show]
+  #
+  resources :events, only: [:new, :create, :show]
   # resources :users, only: [:show]
 end
