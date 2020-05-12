7ab1718be3376ebd0bae46a2c241548af7b9042d
Lebeil <liedel75@gmail.com>
2020-05-12 18:35:15 +0200

    maj mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'liedel75@gmail.com'
+  default from: 'liedel@orange.fr'
 
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
