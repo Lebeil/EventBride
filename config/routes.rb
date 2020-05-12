9adc621709f1c30ea05b48a20247e92cb9618225
Lebeil <liedel75@gmail.com>
2020-05-12 22:54:55 +0200

    maj

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root to: 'events/index'
   # devise_for :users
   # root to: 'events#index'
   # resources :events, only: [:new, :create, :show]
