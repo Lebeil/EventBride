48d9555a444fe70834b222d0aa02eff602acbc80
Lebeil <liedel75@gmail.com>
2020-05-12 23:08:50 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-User.destroy_all
+# Sets the locale to "France":
+Faker::Config.locale = 'fr'
+
+# Détruire la base actuelle
 Event.destroy_all
-Participation.destroy_all
+User.destroy_all
 
-email_array = ["john_dewey@yopmail.com", "judith_butler@yopmail.com", "vandana_shiva@yopmail.com", "jorge_amaldo@yopmail.com", "stefan_zweig@yopmail.com", "alfred_jarry@yopmail.com", "audre_lorde@yopmail.com", "angela_davies@yopmail.com", "james_baldwin@yopmail.com", "william_burroghs@yopmail.com"]
-
-10.times do
-  User.create(
-    email: email_array.sample,
-    encrypted_password: Faker::Code.imei,
-    description: Faker::TvShows::Friends.quote,
-    first_name: Faker::Name.middle_name,
-    last_name: Faker::Name.last_name
-  )
+# Remettre les compteurs à 0
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-duration_array = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-
-10.times do
-  Event.create(
-    start_date: Faker::Date.forward(days: 80),
-    duration: duration_array.sample,
-    title: Faker::Restaurant.description,
-    description: Faker::Restaurant.review,
-    price: Faker::Number.between(from: 1, to: 1000),
-    location: Faker::Address.city,
-    admin_id: User.last.id
-  )
+10.times do |i|
+  User.create(password: "motdepasse", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "crazyeventbritemail0#{i}@yopmail.com", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: true))
 end
 
-10.times do
-  Participation.create(
-    stripe_customer_id: "123456",
-    user_id: User.first.id,
-    event_id: Event.first.id
-  )
+10.times do |i|
+  added_days = rand(1..7)
+  start_date = Time.now + added_days.days
+  Event.create(start_date: start_date, duration: 30, title: Faker::Space.star, description: Faker::ChuckNorris.fact , location: Faker::Restaurant.name, price: rand(1..1000), admin: User.all.sample)
 end
