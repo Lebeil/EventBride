4cf778f489c312aa7565f7ddfd95142fa1d215e5
Lebeil <liedel75@gmail.com>
2020-05-12 19:35:56 +0200

    maj mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,10 +72,12 @@ Rails.application.configure do
       :enable_starttls_auto => true
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_options = { from: 'liedel@yopmail.com' }
   # Send email in development mode.
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
 end
