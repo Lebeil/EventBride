1fc0e01536b1a6c807e6c4a62e09a509dc8bafbe
Lebeil <liedel75@gmail.com>
2020-05-12 17:58:49 +0200

    maj

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,8 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_LOGIN'],
     :password => ENV['SENDGRID_PWD'],
-    :domain => 'monsite.fr',
-    :address => 'smtp.sendgrid.net',
+    :domain => 'https://event75-bride-rails.herokuapp.com/',
+    :address => 'smtp.orange.fr',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
