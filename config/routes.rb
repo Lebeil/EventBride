5c6c2099c803659730f26306fc18511538805d68
Lebeil <liedel75@gmail.com>
2020-05-12 23:21:41 +0200

    maj

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'events/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'events#index'
   # devise_for :users
-
+  # root to: 'events#index'
   # resources :events, only: [:new, :create, :show]
   # resources :users, only: [:show]
 end
