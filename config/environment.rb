1f9477d4ea16a10b7992f007c7f2513c69ca8249
Lebeil <liedel75@gmail.com>
2020-05-13 18:46:25 +0200

    maj mail

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_LOGIN'],
     :password => ENV['SENDGRID_PWD'],
-    :domain => 'herokuapp.com',
+    :domain => 'event75-bride-rails.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
